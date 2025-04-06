4be064c0e4fa06c5b7a7e59e1d91f3a3fd27036d
downey21 <downe7921@gmail.com>
2025-04-06 04:39:40 +0000

    install packages

--- R/separate.R
+++
@@ -1,7 +0,0 @@
-
-# -*- coding: utf-8 -*-
-
-# [Min et al. 2022]
-# Min, Keqian, Qing Mai, and Xin Zhang. "Fast and separable estimation in high-dimensional tensor Gaussian graphical models." *Journal of Computational and Graphical Statistics* 31.1 (2022): 294-300.
-
-rm(list = ls())
